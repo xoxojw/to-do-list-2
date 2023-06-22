213c199c5fdd544a907cd171836964e68f6e8c93
Jiwon Park <xoxojw.dev@gmail.com>
2023-06-22 21:19:36 +0900

    :hammer: TodoItem.jsx 컴포넌트 파일을 TodoList.jsx로 통합

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,19 +1,30 @@
 import React from 'react'
-import { useSelector } from "react-redux";
-
-import TodoItem from "components/TodoItem";
+import { useSelector, useDispatch } from "react-redux";
+import { delete_todo } from "redux/modules/actions";
 
 const TodoList = () => {
   // const todos = (X)
   // const { todos } = (O)
   const { todos } = useSelector((state) => state.todos);
+  console.log(todos);
+
+  const dispatch = useDispatch();
+
+  const onDeleteButtonClickHandler = () => {
+    dispatch(delete_todo(id));
+  }
+    
   return (<>
     {/* TypeError: Cannot read property 'map' of undefined 오류 발생 */}
     {/* 해결방법 : https://tlsdnjs12.tistory.com/56 */}
-    {todos && todos.map((todo) => (
-      <TodoItem key={todo.id} todo={todo} />
-    ))}
+    {todos && todos.map((todo) => {
+      return (
+        <div key={todo.id}>
+          {todo.title}
+        </div>
+      )
+    })}
   </>)
 }
 
-export default TodoList
+export default TodoList;
