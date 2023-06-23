f4e5a43e2cc5a5c64ce587006b6a5b32a94dd5ff
Jiwon Park <xoxojw.dev@gmail.com>
2023-06-23 10:28:10 +0900

    :lipstick: Styled-components 활용하여 전체적인 CSS 수정

--- src/components/AddForm.jsx
+++ src/components/AddForm.jsx
@@ -24,8 +24,8 @@ const AddForm = () => {
   
   return (
     <>
-    <StForm>
-      <form onSubmit={onSubmitHandler}>
+    <section>
+      <StForm onSubmit={onSubmitHandler}>
           <StInput
             name="title"
             type="text"
@@ -42,37 +42,43 @@ const AddForm = () => {
             required
         />
         <StInputButton>제출</StInputButton>
-      </form>
-    </StForm>
+      </StForm>
+    </section>
     </>
   )
 }
 
 export default AddForm;
 
-const StForm = styled.section`
-  padding: 5vh;
-  text-align: center;
-  color: white;
+const StForm = styled.form`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 3vh;
 `;
 
 const StInput = styled.input`
-  width: 25vh;
+  width: 20vh;
   height: 2vh;
-  margin: 5px;
-  padding: 10px;
+  padding: 0.5vh;
+  margin: 0.5vh;
   border: none;
   border-radius: 10px;
   outline: none;
-  text-align: center;
-  background-color: #e7f5ff;
 `;
 
 const StInputButton = styled.button`
   width: 7vh;
-  height: 4vh;
-  border: 1px solid #e7f5ff;
+  height: 3vh;
+  border: none;
   border-radius: 10px;
-  background-color: white;
+  outline: none;
   cursor: pointer;
+  margin-left: 0.5vh;
+  background-color: palevioletred;
+  transition: all 0.2s ease-in-out;
+
+  &:hover {
+    scale: 1.1;
+  }
 `;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import { useSelector } from "react-redux";
 
 import AddForm from "components/AddForm";
 import TodoList from "components/TodoList";
@@ -7,17 +6,24 @@ import TodoList from "components/TodoList";
 import styled from "styled-components";
 
 const Home = () => {
-  const todos = useSelector((state) => state);
-  console.log(todos);
+  const today = new Date();
+  const weekday = ['일', '월', '화', '수', '목', '금', '토']
 
+  const year = today.getFullYear();
+  const month = today.getMonth() + 1;
+  const date = today.getDate();
+  const day = weekday[today.getDay()];
   return (
     <>
-    <StHeader><StTitle>My To-do List</StTitle></StHeader>
-    <main>
-      <AddForm />
-      <TodoList />
-    </main>
-    <StFooter>내일배움캠프 React 6기 To-do List 과제2</StFooter>
+      <StHeader>
+        <StTitle>My To-do List</StTitle>
+        <p className="header-date">{year}년 {month}월 {date}일 {day}요일</p>
+      </StHeader>
+        <StMain>
+          <AddForm />
+          <TodoList />
+        </StMain>
+      <StFooter>내일배움캠프 React 6기 To-do List 과제2</StFooter>
     </>
   )
 }
@@ -25,15 +31,31 @@ const Home = () => {
 export default Home;
 
 const StHeader = styled.header`
-  background-color: salmon;
-  padding: 30px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  border-bottom: 2px solid salmon;
+
+  padding: 2vh;
 `
 
 const StTitle = styled.h1`
-  font-size: 2rem;
+  font-size: 2em;
+  font-weight: 700;
+`
+
+const StMain = styled.main`
+  max-width: 120vh;
+  min-width: 100vh;
+  min-height: 83vh;
+  margin: 2vh auto;
+  border-radius: 20px;
+  background-color: rgba(166, 182, 194, 0.15);
+  backdrop-filter: blur(10px);
+  box-shadow: 2px 7px 15px 8px rgba(0, 0, 0, 0.2);
 `
 
 const StFooter = styled.footer`
-  background-color: skyblue;
-  padding: 30px;
+  padding: 10px;
+  margin: 20px 0 20px 0;
   `;
